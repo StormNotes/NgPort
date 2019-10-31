d4734a65f0339b45455d6a84f6659a9ce783cdcd
Leonardo Diaz <leonardo.diaz@mrm-mccann.com>
2019-10-31 09:15:09 -0300

    for work

--- src/app/port/port.component.ts
+++ src/app/port/port.component.ts
@@ -7,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortComponent implements OnInit {
 
-  constructor() {
+// tslint:disable-next-line: prefer-const
+   dict = {
+    page1:{
+      title:'calculator',
+      img:'../../assets/images/calc.png',
+      desc:'Simple calculator app made with vanilla JS HTML and SCSS',
+      url:'https://stormnotes.github.io/calcu/'
+    },
+    page2:{
+      title:'calculator',
+      desc:'Simple calculator app made with vanilla JS HTML and SCSS',
+      url:'https://stormnotes.github.io/calcu/'
+    }
+  };
+
+constructor() {
 
    }
 
-  ngOnInit() {
+ngOnInit() {
   }
+
 }
