4ccff32a14ce51094ca8110556a5a62fcd10ad88
Leonardo Diaz <leonardo.diaz@mrm-mccann.com>
2019-11-03 12:52:28 -0300

    push to pc

---
+++ src/app/port/pages.ts
@@ -0,0 +1,17 @@
+export const pages = [
+    {
+        pageName: 'Calculator',
+        img:'../../assets/images/calc.png',
+        description:'Small calculator app developed with Vanilla JS'
+    },
+    {
+      pageName: 'Image Galery',
+      img:'../../assets/images/calc.png',
+      description:'Small calculator app developed with Vanilla JS'
+    },
+    {
+      pageName: 'forms',
+      img:'../../assets/images/calc.png',
+      description:'Small calculator app developed with Vanilla JS'
+    }
+]
